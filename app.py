b479562a93bde48ffb35f7bfa7ee1f7b5cfd4dcc
Pragathieswarvy <pragathieswarvy@gmail.com>
2024-08-21 20:25:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import google.generativeai as genai
+
+st.title("Welcome to Gemini chatbot")
+
+genai.configure(api_key="AIzaSyBVIDQ57PbmY5vaAWR5JoYYLCaJTDbq6oE")
+
+text = st.text_input("Enter your Question")
+
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+
+if st.button("Send"):
+        response = chat.send_message(text)
+        st.write(response.text)
+
+
+
